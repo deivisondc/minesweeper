41ae8d7861ee8f30e8970270927607c77b95bb20
Deivison Cardoso <deivisondc@gmail.com>
2020-10-25 17:10:53 -0300

    add global styles

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 
 import MineSweeper from './components/Minesweeper';
 
+import GlobalStyles from './global';
+
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <MineSweeper />
   </React.StrictMode>,
   document.getElementById('root')
