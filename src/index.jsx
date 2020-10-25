641cfc69c51955bf09838f082bc49ffd94863557
Deivison Cardoso <deivisondc@gmail.com>
2020-10-25 17:04:14 -0300

    clean up create-react-app's files

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MineSweeper from './components/Minesweeper';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MineSweeper />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
