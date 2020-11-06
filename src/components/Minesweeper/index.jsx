2d39d09ace8fb74cf5cfe8063e7ddbb002795679
Deivison Cardoso <deivisondc@gmail.com>
2020-11-05 23:06:38 -0300

    remove unecessary import

--- src/components/Minesweeper/index.jsx
+++ src/components/Minesweeper/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { initialize, victory } from '../../store/actions/minesweeperActions';
 
